ce9d416888f8f0f87ee9bf3bb16e8e6a89667233
Wiler Marinez <wilersoriano@gmail.com>
2022-01-14 05:08:20 +0100

    Hidding bg_dark

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-import wallpaper from './../Images/bg_home.jpg'
-import logo from './../Images/logo2.png'
+import wallpaper from './../Images/fondo_ibaiquiz.jpg'
+import logo from './../Images/logo_ibaiquiz.png'
 
 const Home = () => {
     return (
         <Container>
-            <div className="bg_dark"></div>
+            {/* <div className="bg_dark"></div> */}
 
             <div className="menu">
                 <div className="logo">
